381628251d0dbd97ffc758b5ccf754ba653b7f14
Oraida <oraidag@hotmail.com>
2020-01-13 22:48:05 -0600

    usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+/*jshint esversion: 6 */
+
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 
 //Incializar variables
 var app = express();
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //Conexión a la BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) => {
@@ -13,13 +26,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) 
     console.log('Base Datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
-//Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+//rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000, () => {
---
+++ middleware/autenticacion.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+// ==============================
+// verificar el token
+// ==============================
+
+exports.verificaToken = function(req, res, next) {
+
+    var token = req.query.token;
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'Token no valido',
+                errors: err
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+};
